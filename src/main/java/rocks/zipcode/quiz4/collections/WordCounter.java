332e9368a7c558d0b97e52dd89143d21c046a1ab
robertgordon <robertgordon.subscript2@gmail.com>
2019-07-16 09:19:13 -0400

    WordCounter passing tests

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,21 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+
+    private Map<String, Integer> wordCountMap;
+
+
     public WordCounter(String... strings) {
+        wordCountMap = new HashMap<>();
+        for(String str : strings){
+            wordCountMap.merge(str, 1, Integer::sum);
+        }
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        return wordCountMap;
     }
 }
