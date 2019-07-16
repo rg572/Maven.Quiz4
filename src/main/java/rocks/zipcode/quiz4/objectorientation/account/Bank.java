18c5a3cedbe8f681c2f08d3a90bfde98fd60a95a
robertgordon <robertgordon.subscript2@gmail.com>
2019-07-16 10:11:13 -0400

    bank passing tests

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,31 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
+
+    private List<BankAccount> bankAccountList = new ArrayList<>();
+
     public BankAccount removeBankAccountByIndex(Integer indexNumber) {
-        return null;
+        if(indexNumber >= bankAccountList.size()){
+            return null;
+        }
+        else{
+            BankAccount temp = bankAccountList.get(indexNumber);
+            bankAccountList.remove(indexNumber.intValue());
+            return temp;
+        }
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        bankAccountList.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        return bankAccountList.contains(bankAccount);
     }
 }
